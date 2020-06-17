d004bece5d80f4a70fe15b1be439049a3afe9fb4
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-06-18 00:35:40 +0200

    Improved error handling

--- mancala/src/main.rs
+++ mancala/src/main.rs
@@ -6,18 +6,18 @@ use tree_search::tree_search;
 
 fn main() -> Result<(), &'static str> {
     let mut game = Game::new(4)?;
-    let mut move_num = 0;
+    let mut turn_num = 0;
 
     println!("### Game started! ###");
     while let Status::Running = game.status {
         println!("{}", game);
-        println!("move number: {}", move_num);
-        let moves = game.possible_moves();
-        println!("possible moves: {:?}", moves);
-        let m = tree_search(&game, 12);
-        println!("chosing move: {}", m);
+        println!("turn number: {}", turn_num);
+        // let moves = game.possible_moves();
+        // println!("possible moves: {:?}", moves);
+        let m = tree_search(&game, 10)?;
+        println!("chosen move: {}", m);
         game.make_move(m)?;
-        move_num += 1;
+        turn_num += 1;
     }
 
     println!("###  Game ended!  ###");
--- mancala/src/tree_search.rs
+++ mancala/src/tree_search.rs
@@ -3,7 +3,7 @@ use std::i8;
 
 // First player = positive
 // Second player = negative
-fn board_eval(game: Game) -> i8 {
+fn static_eval(game: Game) -> i8 {
     match game.status {
         Status::Running => game.board[6] - game.board[13],
         Status::Ended => match game.get_winner() {
@@ -14,38 +14,41 @@ fn board_eval(game: Game) -> i8 {
     }
 }
 
-pub fn tree_search(game: &Game, depth: u8) -> usize {
+pub fn tree_search(game: &Game, depth: u8) -> Result<usize, &'static str> {
+    // create closure
     let move_eval_closure = |&my_move: &usize| {
         let mut imaginary_game = game.clone();
-        imaginary_game
-            .make_move(my_move)
-            .expect("Err in tree_search");
+        imaginary_game.make_move(my_move)?;
         recursive_tree_search(imaginary_game, 1, depth)
     };
-    let moves = game.possible_moves().into_iter();
+
     // pick best move
-    match game.current_player {
+    let moves = game.possible_moves().into_iter();
+    let best_move = match game.current_player {
         Player::First => moves.max_by_key(move_eval_closure),
         Player::Second => moves.min_by_key(move_eval_closure),
+    };
+    match best_move {
+        Some(m) => Ok(m),
+        None => Err("cannot use tree_search for a game with no moves"),
     }
-    .unwrap()
 }
 
-fn recursive_tree_search(game: Game, depth: u8, max_depth: u8) -> i8 {
+fn recursive_tree_search(game: Game, depth: u8, max_depth: u8) -> Result<i8, &'static str> {
     // end recursion
     if depth >= max_depth {
-        return board_eval(game);
+        return Ok(static_eval(game));
     }
-    // explore
+
     let moves = game.possible_moves();
     if moves.is_empty() {
-        return board_eval(game);
+        return Ok(static_eval(game));
     }
+
+    // explore
     let scores = moves.into_iter().map(|my_move| {
         let mut imaginary_game = game.clone();
-        imaginary_game
-            .make_move(my_move)
-            .expect("Err in recursive_tree_search");
+        imaginary_game.make_move(my_move)?;
         recursive_tree_search(imaginary_game, depth + 1, max_depth)
     });
     match game.current_player {
