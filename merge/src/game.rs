8ded09754f8162a733fffee49a297cea1ed3cc69
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-06-29 20:40:29 +0200

    Added move gen

--- merge/src/game.rs
+++ merge/src/game.rs
@@ -45,7 +45,7 @@ struct Board {
     white: Bitmap<U64>,
     red: Bitmap<U64>,
     l2: Bitmap<U64>,
-    l3: Bitmap<U64>,
+    // l3: Bitmap<U64>,
 }
 
 impl Board {
@@ -54,7 +54,7 @@ impl Board {
             white: Bitmap::from_value(16975631),
             red: Bitmap::from_value(17357084619874238464),
             l2: Bitmap::new(),
-            l3: Bitmap::new(),
+            // l3: Bitmap::new(),
         }
     }
 
@@ -62,18 +62,20 @@ impl Board {
         let mut white = Bitmap::new();
         let mut red = Bitmap::new();
         let mut l2 = Bitmap::new();
-        let mut l3 = Bitmap::new();
+        // let mut l3 = Bitmap::new();
 
         for (i, s) in board.chars().enumerate() {
             // colour
             match s {
                 '1' | '2' | '3' => white.set(i, true),
                 '6' | '7' | '8' => red.set(i, true),
+                _ => true
             };
             // level
             match s {
                 '2' | '7' => l2.set(i, true),
-                '3' | '8' => l3.set(i, true),
+                // '3' | '8' => l3.set(i, true),
+                _ => true
             };
         }
 
@@ -81,7 +83,7 @@ impl Board {
             white,
             red,
             l2,
-            l3,
+            // l3,
         }
     }
 }
@@ -109,14 +111,72 @@ impl Game {
         }
     }
 
-    // fn move_gen(&self) {
-    //     for pos in 0..64 {
-    //     }
-    // }
+    fn move_gen(&self) -> Vec<(usize, usize)> {
+        let my_pieces = match self.current_player {
+            Player::White => &self.board.white,
+            Player::Red => &self.board.red
+        };
 
-    // pub fn make_move(&mut self, mov: (u8, u8)) {
+        let mut moves = Vec::new();
 
-    // }
+        let mut add_moves = |from, x, y, steps| {
+            let left = x > steps - 1;
+            let right = x < 8 - steps;
+            let up = y > steps - 1;
+            let down = y < 8 - steps;
+            
+            if up {
+                moves.push((from, from - 8 * steps));
+                if left {
+                    moves.push((from, from - 9 * steps));
+                }
+                if right {
+                    moves.push((from, from - 7 * steps));
+                }
+            }
+
+            if left {
+                moves.push((from, from - steps));
+            }
+            if right {
+                moves.push((from, from + steps));
+            }
+
+            if down {
+                moves.push((from, from + 8 * steps));
+                if left {
+                    moves.push((from, from + 7 * steps));
+                }
+                if right {
+                    moves.push((from, from + 9 * steps));
+                }
+            }
+        };
+
+        for pos in 0..64 {
+            let x = pos % 8;
+            let y = pos / 8;
+            if my_pieces.get(pos) {
+                add_moves(pos, x, y, 1);
+                add_moves(pos, x, y, 2);
+                if self.board.l2.get(pos) {
+                    add_moves(pos, x, y, 3);
+                }
+            }
+        }
+
+        moves
+    }
+
+    pub fn make_move(&mut self, mov: (usize, usize)) {
+
+    }
+
+    fn update_status(&mut self) {
+        if self.board.white.len() == 1 || self.board.red.len() == 1 {
+            self.status = Status::Ended;
+        }
+    }
 }
 
 fn bitmap_to_string(b: Bitmap<U64>) -> String {
@@ -133,4 +193,47 @@ fn bitmap_to_string(b: Bitmap<U64>) -> String {
         }
     }
     repr
+}
+
+
+
+
+
+
+
+
+
+
+
+fn get_neighbours_of(board: &[u16; 25]) -> Vec<Vec<usize>> {
+    let mut neighbours_of = Vec::new();
+    for i in 0..25 {
+        let x = i % 5;
+        let y = i / 5;
+        let value = board[i];
+        // 0 never appears so we use it for off-limits tiles
+        if value == 0 {
+            neighbours_of.push(Vec::new());
+            continue;
+        }
+        let mut neighbours = Vec::new();
+        // right
+        if x < 4 && value == board[i + 1] {
+            neighbours.push(i + 1);
+        }
+        // down
+        if y < 4 && value == board[i + 5] {
+            neighbours.push(i + 5);
+        }
+        // left
+        if x > 0 && value == board[i - 1] {
+            neighbours.push(i - 1);
+        }
+        // up
+        if y > 0 && value == board[i - 5] {
+            neighbours.push(i - 5);
+        }
+        neighbours_of.push(neighbours);
+    }
+    neighbours_of
 }
