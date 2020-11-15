748188dfffe99baf249d8a51a701d7b62bb43b39
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-11-15 02:09:47 +0100

    Refactored Connection with better error handling

--- onitama/src/connection.rs
+++ onitama/src/connection.rs
@@ -1,12 +1,28 @@
 use crate::messages::*;
+use crate::game::{Move, Game};
+use crate::color::Color;
 use std::marker::Send;
+use std::result::Result;
 use websocket::result::WebSocketResult;
 use websocket::sync::{client::Client, stream::NetworkStream};
 use websocket::{ClientBuilder, Message, OwnedMessage};
 
+macro_rules! recv_loop {
+    ($self:ident, $p:pat => $e:expr) => {
+        loop {
+            match $self.recv() {
+                $p => break $e,
+                Ok(LitamaMessage::Error(msg)) => println!("Received error message: {}", msg.error),
+                Ok(msg) => println!("Received wrong message type {:#?}", msg),
+                Err(err) => println!("Error while receiving: {}", err),
+            }
+        }
+    }
+}
+
 pub struct Participant {
     pub token: String,
-    pub red: bool,
+    pub color: Color,
 }
 
 pub struct Connection {
@@ -14,84 +30,93 @@ pub struct Connection {
 }
 
 impl Connection {
-    pub fn new(address: &str) -> WebSocketResult<Connection> {
+    pub fn new(address: &str) -> Result<Connection, String> {
         let client = ClientBuilder::new(address)
-            .expect("Couldn't parse server address")
-            .connect(None)?;
+            .map_err(|e| e.to_string())?
+            .connect(None)
+            .map_err(|e| e.to_string())?;
         Ok(Connection { client })
     }
 
-    pub fn send(&mut self, text: &str) -> WebSocketResult<()> {
+    fn send(&mut self, text: &str) -> WebSocketResult<()> {
         self.client.send_message(&Message::text(text))
     }
 
-    pub fn recv(&mut self) -> WebSocketResult<LitamaMessage> {
-        let message = match self.client.recv_message()? {
-            // if it's a parse error then the server protocol has probably changed and retrying won't help
-            OwnedMessage::Text(text) => serde_json::from_str::<LitamaMessage>(&text).unwrap(),
-            OwnedMessage::Binary(bytes) => serde_json::from_slice::<LitamaMessage>(&bytes).unwrap(),
-            // another message that is not handled
-            // OwnedMessage::Ping(_) => panic!("todo"),
-            // OwnedMessage::Pong(_) => panic!("todo"),
-            // OwnedMessage::Close(_) => panic!("todo"),
-            msg => panic!("Received unexpected message: {:#?}", msg),
+    fn recv(&mut self) -> WebSocketResult<LitamaMessage> {
+        let message = loop {
+            match self.client.recv_message()? {
+                // if it's a parse error then the server protocol has probably changed and retrying won't help
+                OwnedMessage::Text(text) => {
+                    println!("{}", &text);
+                    break serde_json::from_str::<LitamaMessage>(&text).unwrap();
+                }
+                OwnedMessage::Binary(bytes) => {
+                    break serde_json::from_slice::<LitamaMessage>(&bytes).unwrap()
+                }
+                // another message that is not handled
+                OwnedMessage::Ping(data) => {
+                    self.client.send_message(&OwnedMessage::Pong(data)).unwrap()
+                }
+                // OwnedMessage::Pong(_) => panic!("todo"),
+                // OwnedMessage::Close(_) => panic!("todo"),
+                msg => panic!("Received unexpected message: {:#?}", msg),
+            }
         };
         Ok(message)
     }
 
-    pub fn create_match(&mut self) -> (String, Participant) {
+    pub fn create_match(&mut self, username: &str) -> (String, Participant) {
         // send create message
-        while let Err(err) = self.send("create") {
+        while let Err(err) = self.send(&format!("create {}", username)) {
             println!("Error while sending: {}", err);
         }
-        // try to receive response
-        loop {
-            match self.recv() {
-                Ok(LitamaMessage::Create(msg)) => {
-                    return (
-                        msg.match_id,
-                        Participant {
-                            token: msg.token,
-                            red: color_is_red(msg.color).unwrap(),
-                        },
-                    )
-                }
-                Ok(LitamaMessage::Error(msg)) => println!("Received error message: {}", msg.error),
-                Ok(msg) => println!("Received wrong message type {:#?}", msg),
-                Err(err) => println!("Error while receiving: {}", err),
-            }
-        }
+        return recv_loop!(
+            self,
+            Ok(LitamaMessage::Create(msg)) => (
+                msg.match_id,
+                Participant {
+                    token: msg.token,
+                    color: Color::from(msg.color).unwrap(),
+                },
+            )
+        )
     }
 
-    pub fn join_match(&mut self, match_id: &str) -> Participant {
+    pub fn join_match(&mut self, match_id: &str, username: &str) -> Participant {
         // send join message
-        let to_send_msg = format!("join {}", match_id);
-        while let Err(err) = self.send(&to_send_msg) {
+        while let Err(err) = self.send(&format!("join {} {}", match_id, username)) {
             println!("Error while sending: {}", err)
         }
-        // try to receive response
-        loop {
-            match self.recv() {
-                Ok(LitamaMessage::Join(msg)) => {
-                    return Participant {
-                        token: msg.token,
-                        red: color_is_red(msg.color).unwrap(),
-                    }
-                }
-                Ok(LitamaMessage::Error(msg)) => println!("Received error message: {}", msg.error),
-                Ok(msg) => println!("Received wrong message type {:#?}", msg),
-                Err(err) => println!("Error while receiving: {}", err),
+        return recv_loop!(
+            self,
+            Ok(LitamaMessage::Join(msg)) => Participant {
+                token: msg.token,
+                color: Color::from(msg.color).unwrap(),
             }
+        )
+    }
+
+    pub fn spectate(&mut self, match_id: &str) -> StateMsg {
+        // send spectate message
+        while let Err(err) = self.send(&format!("spectate {}", match_id)) {
+            println!("Error while sending: {}", err)
         }
+        // confirm spectate
+        recv_loop!(self, Ok(LitamaMessage::Spectate(_)) => ());
+        self.recv_state()
     }
 
     pub fn recv_state(&mut self) -> StateMsg {
-        loop {
-            match self.recv() {
-                Ok(LitamaMessage::State(msg)) => return msg,
-                Ok(msg) => println!("Received wrong message type {:#?}", msg),
-                Err(err) => println!("Error while receiving: {}", err),
-            }
+        return recv_loop!(self, Ok(LitamaMessage::State(msg)) => msg)
+    }
+
+    pub fn make_move(&mut self, my_move: &Move, match_id: &str, token: &str, game: &Game) -> StateMsg {
+        // send move
+        while let Err(err) = self.send(&move_to_command(my_move, match_id, token, game)) {
+            println!("Error while sending: {}", err)
         }
+        // confirm move
+        recv_loop!(self, Ok(LitamaMessage::Move(_)) => ());
+        self.recv_state()
     }
 }
